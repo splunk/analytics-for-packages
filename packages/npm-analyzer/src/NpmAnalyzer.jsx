375ba3750dbd1fe84895e764f26fc273815e7ab7
Ryan O'Connor <ryan.oconnor@gmail.com>
2021-11-22 11:24:15 -0500

    Added ability to get data from Github packages as well

--- packages/npm-analyzer/src/NpmAnalyzer.jsx
+++ packages/npm-analyzer/src/NpmAnalyzer.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '@splunk/react-ui/Button';
 import Text from '@splunk/react-ui/Text';
+import Select from '@splunk/react-ui/Select';
+
 import Line from '@splunk/visualizations/Line';
 
 import { StyledContainer } from './NpmAnalyzerStyles';
@@ -18,7 +20,7 @@ class NpmAnalyzer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: "Click 'Get Downloads' to Get Results", current_package: "@splunk/create", dates: [''], downloads: ['']
+            message: "Click 'Get Downloads' to Get Results", fetch_error: "", github_token: "", github_username: "", package_source: "NPM", current_package: "@splunk/create", dates: [''], downloads: ['']
         };
     }
     render() {
@@ -26,39 +28,117 @@ class NpmAnalyzer extends Component {
         const { downloads } = this.state;
         const { dates } = this.state;
         const { message } = this.state;
+        const { package_source } = this.state;
+        const { github_token } = this.state;
+        const { github_username } = this.state;
+        const { fetch_error } = this.state;
+
+
+
         const handleChange = (e, { value }) => {
             this.setState({ current_package: value });
         };
 
+        const handleRepoChange = (e, { value }) => {
+            this.setState({ package_source: value });
+        };
+        const handleGithubUsernameChange = (e, { value }) => {
+            this.setState({ github_username: value });
+        };
+        const handleGithubTokenChange = (e, { value }) => {
+            this.setState({ github_token: value });
+        };
+
+        const toggle = <Button appearance="toggle" label="Select Package Source" isMenu />;
+
         const handleClick = async () => {
+
+            this.setState({dates: ['']})
+            this.setState({downloads: ['']})
 
             var new_downloads = []
             var new_dates = []
-            const response = await fetch("https://api.npmjs.org/downloads/range/last-year/" + current_package)
-                .then(response => response.json())
-                .then(result => {
-                    return result
-                })
-                .catch(e => {
-                    console.log(e);
-                    this.setState({ ...this.state, isFetching: false });
-                });
-            this.setState({ data: response.downloads })
 
-            for (const value of response.downloads) {
-                new_downloads.push(value.downloads)
-                new_dates.push(value.day)
+            var response = ""
+            if (package_source == "NPM") {
+                response = await fetch("https://api.npmjs.org/downloads/range/last-year/" + current_package)
+                    .then(response => {
+                        if (response.ok) {
+                            this.setState({fetch_error:""})
+                            return response.json()
+                        }
+                        else{
+                            throw new Error('Something went wrong');
+                        }
+                    })
+                    .then(result => {
+                        
+                            return result
+                    })
+                    .catch(e => {
+                        this.setState({ fetch_error: "Error Fetching Data. " })
+                        this.setState({ ...this.state, isFetching: false });
+                    });
+
+
+                for (const value of response.downloads) {
+                    new_downloads.push(value.downloads)
+                    new_dates.push(value.day)
+                }
+                this.setState({ downloads: new_downloads })
+                this.setState({ dates: new_dates })
+                this.setState({ message: "" })
             }
-            this.setState({ downloads: new_downloads })
-            this.setState({ dates: new_dates })
-            this.setState({ message: "" })
+            if (package_source == "Github") {
+                response = await fetch("https://api.github.com/repos/" + current_package + "/traffic/clones", {
+                    headers: {
+                        'Authorization': 'Basic ' + btoa(github_username + ":" + github_token)
+                    }
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            this.setState({fetch_error:""})
+                            return response.json()
+                        }
+                        else{
+                            throw new Error('Something went wrong');
+                        }
+                    })
+                    .then(result => {
+                        return result
+                    })
+                    .catch(e => {
+                        console.log("ERRORRRRR")
+                        this.setState({ fetch_error: "Error Fetching Data. Verify you have a personal access token and it has push permissions to the repository" })
+                        this.setState({ ...this.state, isFetching: false });
+                    });
 
+                for (const value of response.clones) {
+                    new_downloads.push(value.count)
+                    new_dates.push(value.timestamp)
+                }
+                this.setState({ downloads: new_downloads })
+                this.setState({ dates: new_dates })
+                this.setState({ message: "" })
+            }
+        }
+
+        var github_entry = ""
+        if (package_source == "Github") {
+            github_entry = <><Text defaultValue="Username" canClear onChange={handleGithubUsernameChange} /> <Text defaultValue="Personal Access Token" canClear onChange={handleGithubTokenChange} /></>
         }
 
         return (
-            <StyledContainer stye={{width:'100%'}}>
-                <div>Enter NPM Package:</div>
+            <StyledContainer stye={{ width: '100%' }}>
+                <div>Enter NPM Package or Github Repo (This should include org/owner and the repo name. For example: @splunk/create or ):</div>
                 <Text defaultValue="@splunk/create" canClear onChange={handleChange} />
+                <p>{fetch_error}</p>
+                <Select value={package_source} onChange={handleRepoChange}>
+                    <Select.Option label="Github" value="Github" />
+                    <Select.Option label="NPM" value="NPM" />
+                </Select>
+                {github_entry}
+
                 <Button
                     label="Get Downloads"
                     appearance="primary"
@@ -90,9 +170,7 @@ class NpmAnalyzer extends Component {
                         },
                     }}
                 />
-
             </StyledContainer>
-
         );
     }
 }
